0ab707a1260e6e438259ad46f1d85ab5e82bdab9
importZL <include.zl.h@gmail.com>
2023-01-11 17:00:52 -0800

    update_train_pix2pix_unet

--- models_pix2pix/base_model.py
+++ models_pix2pix/base_model.py
@@ -122,7 +122,7 @@ class BaseModel(ABC):
                 scheduler.step()
 
         lr = self.optimizers[0].param_groups[0]['lr']
-        print('learning rate %.7f -> %.7f' % (old_lr, lr))
+        # print('learning rate %.7f -> %.7f' % (old_lr, lr))
 
     def get_current_visuals(self):
         """Return visualization images. train.py will display these images with visdom, and save the images to a HTML"""
