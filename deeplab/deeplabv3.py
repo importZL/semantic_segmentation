5beb9fcdc990a4dde304c44258bdd79a37d43d2d
importZL <include.zl.h@gmail.com>
2025-03-11 06:21:48 +0000

    update

--- deeplab/deeplabv3.py
+++ deeplab/deeplabv3.py
@@ -4,8 +4,7 @@ class DeepLabV3(torch.nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         torchvision_version = 'pytorch/vision:v0.14.1'
-        self.n_classes = num_classes
-        
+        self.n_classes = num_classes        
         self.main = torch.hub.load(torchvision_version, 'deeplabv3_resnet50', num_classes=num_classes, weights=None)
     
     def forward(self, x):
