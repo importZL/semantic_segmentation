bd6180e32420d14a3f24dae98535ec67d8ac9bd1
importZL <include.zl.h@gmail.com>
2025-03-11 06:53:35 +0000

    update

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
-conda create -n semantic python==3.9
-conda activate semantic
+conda create -n GenSeg python==3.9
+conda activate GenSeg
 pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
 pip install imgaug
 pip install numpy==1.23
